feb2c874a48192a0bd7e58deb05b831de19e640e
HateOne <147848015+HateOne@users.noreply.github.com>
2023-11-11 22:08:58 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int amount = 13676;
+        int percent = 5;
 
-            int amount = 13676;
-            int percent = 5;
+        int bonus = amount / 100 * percent;
 
-            int bonus = amount / 100 * percent;
-
-            System.out.println("Итоговый бонус:" + bonus);
-        }
+        System.out.println("Итоговый бонус:" + bonus);
     }
+}
